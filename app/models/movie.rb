b9c48c1a561920a568dce4c97d39983aacbe758e
zeyad saleh <zeyadmostafasaleh@gmail.com>
2024-02-03 16:46:34 +0200

    import rake file for movies and reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,5 @@ class Movie < ApplicationRecord
   belongs_to :city
   has_many :reviews, dependent: :nullify
   validates_presence_of :name
-  validates :release_year, allow_blank: true, numericality: { only_integer: true, greater_than_or_equal_to: 1800, less_than_or_equal_to: Time.current.year }
+  validates :year, allow_blank: true, numericality: { only_integer: true, greater_than_or_equal_to: 1800, less_than_or_equal_to: Time.current.year }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     validates_presence_of :name
     has_many :reviews, dependent: :nullify
+
+
+    #alias_attribute "Movie", :name
+
+
 end
