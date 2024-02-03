be813a378faf048cbe1c02bc7258b5c3ff63d1a9
zeyad saleh <zeyadmostafasaleh@gmail.com>
2024-02-03 17:10:13 +0200

    search and reviews using ransack

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,12 @@ class Movie < ApplicationRecord
   has_many :reviews, dependent: :nullify
   validates_presence_of :name
   validates :year, allow_blank: true, numericality: { only_integer: true, greater_than_or_equal_to: 1800, less_than_or_equal_to: Time.current.year }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["actor_id", "city_id", "created_at", "desc", "director_id", "id", "id_value", "name", "updated_at", "year"]
+  end
+
+  def average_reviews
+    reviews.average(:rate)
+  end
 end
