84db0947193da2290835fb49c83ae01df4c7d7d8
zeyad saleh <zeyadmostafasaleh@gmail.com>
2024-02-03 18:06:38 +0200

    sort by avarage movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,8 +9,24 @@ class Movie < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["actor_id", "city_id", "created_at", "desc", "director_id", "id", "id_value", "name", "updated_at", "year"]
   end
+  def self.ransackable_associations(auth_object = nil)
+    ["actor", "city", "director", "reviews"]
+    end
 
   def average_reviews
     reviews.average(:rate)
   end
+
+  scope :sort_by_average_reviews_asc, -> {
+    select('movies.*,AVG(reviews.rate)').
+    joins('LEFT JOIN reviews ON movies.id = reviews.movie_id')
+      .group('movies.id')
+      .order('AVG(reviews.rate) ASC NULLS LAST')
+  }  
+  scope :sort_by_average_reviews_desc, -> {
+    select('movies.*,AVG(reviews.rate)').
+    joins('LEFT JOIN reviews ON movies.id = reviews.movie_id')
+      .group('movies.id')
+      .order('AVG(reviews.rate) DESC NULLS LAST')
+  }  
 end
