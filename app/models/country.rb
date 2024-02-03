8478d4617145de9ea805e1e49f367e42273418ce
zeyad saleh <zeyadmostafasaleh@gmail.com>
2024-02-03 15:10:56 +0200

    model relations and validations

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,4 @@
 class Country < ApplicationRecord
+    validates_presence_of :name
+    has_many :cities, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates_presence_of :name
+    has_many :reviews, dependent: :nullify
 end
